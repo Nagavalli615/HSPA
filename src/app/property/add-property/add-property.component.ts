05d77d9b83890df6a94e751c1eea9d43c7843447
Nagavalli <vallikatta226@gmail.com>
2024-02-28 17:07:43 +0530

    HSPA

---
+++ src/app/property/add-property/add-property.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
+import { IProperty } from '../IProperty.interface';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm: NgForm;
+  @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  // Will come from masters
+  propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex']
+  furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished']
+
+  propertyView: IProperty = {
+    Id: null,
+    Name: '',
+    Price: null,
+    SellRent: null,
+    Type: null
+  };
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onBack() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('Congrats, form Submitted');
+    console.log(this.addPropertyForm);
+  }
+
+  selectTab(tabId: number) {
+    this.formTabs.tabs[tabId].active = true;
+  }
+
+}
